afe8ed83e494d3320fa6134f58b95322d93b820e
ShadowalRazack <122894326+ShadowalRazack@users.noreply.github.com>
2023-03-22 12:02:30 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,54 @@
-const math = require("./math.js");
+const { getRandomStudent, getAllStudents, getStudentByOrderNumber, getStudentsCount, addStudent } = require("./students");
 
-console.log(math.add(5, 4)); // Tulemus: 9
-console.log(math.subtract(9, 2)); // Tulemus: 7
-console.log(math.multiply(9, 3)); // Tulemus: 27
-console.log(math.divide(10, 5)); // Tulemus: 2
+console.log("Õpilaste arv: ", getStudentsCount());
+const students = [
+    "Sirle",
+    "Alvaro",
+    "Victoria",
+    "Engo",
+    "Haldi",
+    "Kalle",
+    "Eva",
+    "Kasper",
+    "Doris",
+    "Madis"
+]
+
+function getStudentsCount() {
+    return students.length;
+}
+
+function getRandomStudent() {
+    const randomIndex = Math.floor(Math.random() * students.length);
+    return students[randomIndex];
+}
+
+function getAllStudents() {
+    return students;
+}
+
+function getStudentByOrderNumber(orderNumber) {
+    try {
+        if (orderNumber < 0 || orderNumber >= students.length) {
+            return null;
+        }
+        return students[orderNumber - 1];
+    } catch (error) {
+        console.error(error.message);
+        return null;
+    }
+}
+
+function addStudent(name) {
+    students.push(name);
+    return students.length;
+}
+
+
+module.exports = {
+    getRandomStudent,
+    getAllStudents,
+    getStudentByOrderNumber,
+    addStudent,
+    getStudentsCount
+};
