f89d4fc47184f737ede2e58016a6f3e423718a91
ShadowalRazack <122894326+ShadowalRazack@users.noreply.github.com>
2023-03-22 12:01:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const math = require("./math.js");
+
+console.log(math.add(5, 4)); // Tulemus: 9
+console.log(math.subtract(9, 2)); // Tulemus: 7
+console.log(math.multiply(9, 3)); // Tulemus: 27
+console.log(math.divide(10, 5)); // Tulemus: 2
---
+++ math.js
@@ -0,0 +1,7 @@
+const math = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+};
+module.exports = math;
